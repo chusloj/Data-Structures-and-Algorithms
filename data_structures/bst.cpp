ccec5f49a049f5ecdf546771befe514197b9d76a
chusloj <chusloj@gmail.com>
2021-11-03 19:41:24 -0700

    add binary search tree basic implementation

---
+++ data_structures/bst.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+
+struct BstNode {
+	int data;
+	BstNode* left;
+	BstNode* right;
+};
+
+BstNode* GetNewNode(int x) {
+	BstNode* newNode = new BstNode;
+	newNode->data = x;
+	newNode->left = newNode->right = NULL;
+	return newNode;
+}
+
+// recursive approach
+BstNode* Insert(BstNode* root, int x) {
+	if(root == NULL) {
+		root = GetNewNode(x);
+	}
+	else if(x <= root->data) {
+		root->left = Insert(root->left, x);
+	}
+	else {
+		root->right = Insert(root->right, x);
+	}
+	return root;
+}
+
+bool Search(BstNode* root, int y) {
+	if(root == NULL) {return false;}
+	else if(root->data == y) {return true;}
+	else if(y <= root->data) {return Search(root->left, y);}
+	else {return Search(root->right, y);}
+}
+
+
+int main() {
+	BstNode* root = NULL;
+	root = Insert(root, 5);
+	root = Insert(root, 15);
+	root = Insert(root, 10);
+	root = Insert(root, 20);
+	root = Insert(root, 25);
+	std::cout << Search(root, 25) << std::endl;
+}
